be7e864fa11e8d2bc84f14a04c02a48efa40ab71
ErlBal <erlan310706@gmail.com>
2025-05-31 20:03:57 +0300

    some upgrades

--- src/utils/auth.ts
+++
@@ -1,8 +0,0 @@
-export function isLoggedIn() {
-  return !!localStorage.getItem('jwtToken');
-}
-
-export function logout() {
-  localStorage.removeItem('jwtToken');
-  localStorage.removeItem('playerId');
-} 
